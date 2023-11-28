f6ff85a581e4bd133f6e94601ce1af5152a46985
Nicolai <nicolai.krekiehn@googlemail.com>
2023-11-28 23:02:06 +0100

    debug relative modul imports

--- Code/MONAI/vanilla_training.py
+++ Code/MONAI/vanilla_training.py
@@ -36,7 +36,9 @@ from datetime import datetime
 
 # from multiprocessing import Process, freeze_support, set_start_method
 import sys
-sys.path.append('Code')
+sys.path.append(os.getcwd())
+print(os.getcwd())
+print(sys.path)
 
 from Code.MONAI.CustomTransforms import ReplaceValuesNotInList
 from Code.MONAI.DataLoader import get_data_dicts, check_transforms_in_dataloader
