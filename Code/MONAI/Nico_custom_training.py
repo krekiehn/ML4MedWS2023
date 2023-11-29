ac02f1a774ca880abb7406f22bd7135610a2635c
Nicolai <nicolai.krekiehn@googlemail.com>
2023-11-29 12:53:16 +0100

    reduced Batchsize due to OOM error

--- Code/MONAI/Nico_custom_training.py
+++ Code/MONAI/Nico_custom_training.py
@@ -84,7 +84,7 @@ if debug_mode:
     train_files, val_files = get_data_dicts(stop_index=BATCH_SIZE)
 else:
     # User Mode
-    BATCH_SIZE = 32
+    BATCH_SIZE = 16
     MAX_EPOCHS = 600
     VAL_INTERVAL = 1
 
